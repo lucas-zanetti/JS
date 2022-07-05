30c21604cfe1f53b788a991e8761ed3c4169de15
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-05 01:16:12 -0300

    Adding calculator, CalcController and intanstianting it

---
+++ calculator/scripts/controller/CalcController.js
@@ -0,0 +1,71 @@
+class CalcController{
+
+    constructor(){
+        this._locale = navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        this._currentDate;
+        this.initialize();
+        this.initButtonsEvent();
+    }
+
+    initialize() {
+        this.setDisplayDateTime();
+        
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);        
+    }
+
+    initButtonsEvent(){
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index)=>{
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+        });
+    }
+
+    setDisplayDateTime(){
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+}
