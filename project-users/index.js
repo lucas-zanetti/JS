ab2c1572f5caee6281dea8c17f28b54c97d8767d
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-07 16:07:36 -0300

    Adding user from object to user table

--- project-users/index.js
+++ project-users/index.js
@@ -1,14 +1,33 @@
 var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
-fields.forEach((field, index) => {
-    if (field.name == 'gender'){
-        if (field.checked){
+function addLine(dataUser){
+    document.getElementById('table-users').innerHTML = `
+        <tr>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`;
+}
+
+document.getElementById('form-user-create').addEventListener('submit', event =>{
+    event.preventDefault();
+
+    fields.forEach((field, index) => {
+        if (field.name == 'gender'){
+            if (field.checked){
+                user[field.name] = field.value;
+            }
+        } else {
             user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
-});
-
-console.log(user);
+    });
+    
+    addLine(user);
+})
