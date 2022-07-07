4ed1d1c6cdf8fa27bb1b248f46edeeabd1ac74b4
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-06 23:18:13 -0300

    Adding audio to calculator

--- calculator/scripts/controller/CalcController.js
+++ calculator/scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController{
 
     constructor(){
+        this._audio = new Audio('click.mp3')
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -9,6 +11,7 @@ class CalcController{
         this._dateEl = document.querySelector('#date');
         this._timeEl = document.querySelector('#time');
         this._currentDate;
+
         this.initialize();
         this.initButtonsEvent();
         this.initKeyboard();
@@ -31,10 +34,29 @@ class CalcController{
         }, 1000);        
 
         this.setLastNumberToDisplay();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+        if(this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard(){
         document.addEventListener('keyup', e=> {
+            this.playAudio();
+
             switch(e.key){
                 case 'Escape':
                     this.clearAll();
@@ -220,6 +242,8 @@ class CalcController{
     }
 
     execBtn(value){
+        this.playAudio();
+
         switch(value){
             case 'ac':
                 this.clearAll();
