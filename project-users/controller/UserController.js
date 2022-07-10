4b74e306cb40fdb05568a6f04f02643a821342c1
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-09 23:56:27 -0300

    Adding validation on HTML and JS for Name, Email and Password

--- project-users/controller/UserController.js
+++ project-users/controller/UserController.js
@@ -17,6 +17,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
                 (content)=>{
                     values.photo = content;
@@ -62,8 +64,15 @@ class UserController {
 
     getValues(){
         let user = {};
+        let isValid = true;
         
         [...this.formEl.elements].forEach((field, index) => {
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+
+                isValid = false;
+            }
+
             if (field.name == 'gender'){
                 if (field.checked){
                     user[field.name] = field.value;
@@ -74,6 +83,8 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
+
+        if (!isValid) return false;
     
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
         
