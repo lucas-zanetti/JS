b32c2fc61c149ff34d9531c92a4c5b18b8d7a44e
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-13 23:47:02 -0300

    Adding bodyParser

--- project-node/routes/users.js
+++ project-node/routes/users.js
@@ -16,14 +16,12 @@ module.exports = (app) => {
         });
     });
     
-    app.get('/users/admin', (req, res)=>{
+    app.post('/users', (req, res)=>{
         console.log('URL: ', req.url);
         console.log('METHOD: ', req.method);
     
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: []
-        });
+        res.json(req.body);
     });
 }
