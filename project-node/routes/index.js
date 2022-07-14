3b32d3e8934ab954cc94c66e54d110929eb26aee
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-13 23:24:28 -0300

    Adding consign to read routes

--- project-node/routes/index.js
+++ project-node/routes/index.js
@@ -1,13 +1,10 @@
-let express = require('express');
-let routes = express.Router();
-
-routes.get('/', (req, res)=>{
-    console.log('URL: ', req.url);
-    console.log('METHOD: ', req.method);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Olá!</h1>');
-});
-
-module.exports = routes;
+module.exports = (app) => {
+    app.get('/', (req, res)=>{
+        console.log('URL: ', req.url);
+        console.log('METHOD: ', req.method);
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Olá!</h1>');
+    });
+}
--- project-node/routes/users.js
+++ project-node/routes/users.js
@@ -1,32 +1,29 @@
-let express = require('express');
-let routes = express.Router();
-
-routes.get('', (req, res)=>{
-    console.log('URL: ', req.url);
-    console.log('METHOD: ', req.method);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: [
-            {
-                name: "Lucas Zanetti",
-                email: "lucas.hzanetti@gmail.com",
-                id: 1
-            }
-        ]
+module.exports = (app) => {
+    app.get('/users', (req, res)=>{
+        console.log('URL: ', req.url);
+        console.log('METHOD: ', req.method);
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [
+                {
+                    name: "Lucas Zanetti",
+                    email: "lucas.hzanetti@gmail.com",
+                    id: 1
+                }
+            ]
+        });
     });
-});
-
-routes.get('/admin', (req, res)=>{
-    console.log('URL: ', req.url);
-    console.log('METHOD: ', req.method);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users: []
+    
+    app.get('/users/admin', (req, res)=>{
+        console.log('URL: ', req.url);
+        console.log('METHOD: ', req.method);
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: []
+        });
     });
-});
-
-module.exports = routes;
+}
