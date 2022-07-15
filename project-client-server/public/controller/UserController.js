e5b96db3b0158fd43534f64c38c31dd0f1fa8243
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-15 00:10:26 -0300

    Adding new class HttpRequest

--- project-client-server/public/controller/UserController.js
+++ project-client-server/public/controller/UserController.js
@@ -149,29 +149,15 @@ class UserController {
     }
 
     selectAll(){
-        let ajax = new XMLHttpRequest();
-
-        ajax.open('GET', '/users');
-
-        ajax.onload = event =>{
-            let obj = { users: [] };
-            
-            try{
-                obj = JSON.parse(ajax.responseText);
-            } catch (e){
-                console.log(e);
-            }            
-
-            obj.users.forEach(dataUser=>{
+        HttpRequest.get('/users').then(data =>{
+            data.users.forEach(dataUser=>{
                 let user = new User();
     
                 user.loadFromJSON(dataUser);
     
                 this.addLine(user);
             });
-        };
-
-        ajax.send();
+        });
     }
 
     addLine(dataUser){
