3bbef781608d60dc798e10b379f9ddd2e8eadf7f
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-07-27 22:51:10 -0300

    Adding method to rename files

--- project-dropbox/app/public/src/controller/DropBoxController.js
+++ project-dropbox/app/public/src/controller/DropBoxController.js
@@ -42,6 +42,20 @@ class DropBoxController{
     }
 
     initEvents(){
+        this.btnRenameEl.addEventListener('click', e=>{
+            let li = this.getSelection()[0];
+
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Renomear o arquivo: ", file.originalFilename);
+
+            if (name){
+                file.originalFilename = name;
+
+                this.getFirebaseRef().child(li.dataset.key).set(file);
+            }
+        });
+
         this.listFilesEl.addEventListener('selectionchange', e=>{
             switch (this.getSelection().length){
                 case 0:
@@ -322,6 +336,7 @@ class DropBoxController{
         let li = document.createElement('li');
 
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
 
         li.innerHTML = `${this.getFileIconView(file)}
                         <div class="name text-center">${file.originalFilename}</div>`;
